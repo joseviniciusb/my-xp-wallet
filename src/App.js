3c55ea1a703e07cc6ed16006e429d3173e646c2b
José Vinicius <josevbluz@outlook.com>
2022-07-15 17:09:07 -0300

    Installing Material Ui framework

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
+  
   return (
     <div className="App">
       <h3>my first wallet text</h3>
+      <Button variant="contained"> Testing Material Ui...</Button>;
     </div>
   );
 }
