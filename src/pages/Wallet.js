8f96a176b335fe84a4f76592b1c90ca5c6379d7c
joseviniciusb <josevbluz@outlook.com>
2022-07-20 17:26:41 -0300

    feat: table constructing

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,58 @@
 import * as React from "react";
-import Header from '../components/Header'
+import Header from "../components/Header";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
+function createData(name, quantidade, preço, negociar) {
+  return { name, quantidade, preço, negociar };
+}
 
+const rows = [
+  createData("BBDC4", 159, 6.0, "Comprar / Vender"),
+  createData("BRAP4", 237, 9.0, "Comprar / Vender"),
+  createData("BBAS3", 262, 16.0, "Comprar / Vender"),
+  createData("BTC", 305, 3.7, "Comprar / Vender"),
+  createData("Lalaland", 356, 16.0, "Comprar / Vender"),
+];
 
 const Wallet = () => {
-
-
   return (
     <>
-    <Header />
-
+      <Header />
+      <TableContainer>
+        <Table
+          sx={{ minWidth: 650, maxWidth: 700, my: 10, mx: 35 }}
+          size="medium"
+          aria-label="a dense table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Qtd</TableCell>
+              <TableCell>Valor</TableCell>
+              <TableCell>Negociar</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name} sx={{ my: "100" }}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>{row.quantidade}</TableCell>
+                <TableCell>{row.preço}</TableCell>
+                <TableCell>{row.negociar}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
