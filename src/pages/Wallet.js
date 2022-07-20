8fc8ca63abbbec1e3221b4141e2609d04387e210
joseviniciusb <josevbluz@outlook.com>
2022-07-20 15:06:10 -0300

    feat: material Ui icons install and new feature for dashboard

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,49 @@
-import React from "react";
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import GrainIcon from "@mui/icons-material/Grain";
 
 function Wallet() {
-  return <h1>Lalaland</h1>;
-}
+  function handleClick(event) {
+    console.info("You clicked a breadcrumb.");
+  }
 
-export default Wallet
+  return (
+    <>
+      <header>Xp Wallet</header>
+      <div role="presentation" onClick={handleClick}>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link
+            underline="hover"
+            sx={{ display: "flex", alignItems: "center" }}
+            color="inherit"
+            href="/"
+          >
+            <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            MUI
+          </Link>
+          <Link
+            underline="hover"
+            sx={{ display: "flex", alignItems: "center" }}
+            color="inherit"
+            href="/material-ui/getting-started/installation/"
+          >
+            <WhatshotIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            Core
+          </Link>
+          <Typography
+            sx={{ display: "flex", alignItems: "center" }}
+            color="text.primary"
+          >
+            <GrainIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            Breadcrumb
+          </Typography>
+        </Breadcrumbs>
+      </div>
+    </>
+  );
+}
+export default Wallet;
